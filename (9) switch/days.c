cb76b03c30ad1f989ad0697c6f4575d7c247a687
RehanAnsari765 <ra6369419@gmail.com>
2024-11-14 11:57:44 +0530

    Add files via upload

---
+++ (9) switch/days.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+void main () {
+
+    int day = 7;
+
+    switch (day) {
+
+    case 1:
+    printf("SUNDAY");
+    break;
+
+    case 2:
+    printf("MONDAY");
+    break;
+
+    case 3:
+    printf("TUESDAY");
+    break;
+
+    case 4:
+    printf("WEDNESDAY");
+    break;
+
+    case 5:
+    printf("THURSDAY");
+    break;
+
+    case 6:
+    printf("FRIDAY");
+    break;
+
+    case 7:
+    printf("SATDAY");
+    break;
+
+    default:
+    printf("No Day Found");
+
+    }
+}

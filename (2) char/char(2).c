89f32f5386385324615654330cea8d996e33488a
RehanAnsari765 <ra6369419@gmail.com>
2024-11-14 11:47:29 +0530

    Add files via upload

---
+++ (2) char/char(2).c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void main () {
+
+ char a [15];
+
+ printf("Enter your name : ");
+ scanf("%s", a);
+
+ printf("Your name is : %s\n", a);
+
+}
---
+++ (2) char/char.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main (){
+
+char a [10] = "Rehan";
+printf("%s", a);
+
+}

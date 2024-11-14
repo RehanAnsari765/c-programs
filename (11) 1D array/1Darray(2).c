bc470f79f73da11edf75a4c9828a8647648616b1
RehanAnsari765 <ra6369419@gmail.com>
2024-11-14 11:59:00 +0530

    Add files via upload

---
+++ (11) 1D array/1Darray(2).c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main () {
+
+    int a [10] = {100, 200, 300, 400, 500, 600, 700, 800, 900, 1000};
+
+    for (int i=0; i < 7; i++) {
+        printf ("%d\n", a[i]);
+    }
+}
---
+++ (11) 1D array/1Darraychar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main () {
+
+    char a[5] = {'R','E','H','A','N'};
+
+    for (int i = 0; i < 5; i++) {
+        printf("%c", a[i]);
+    }
+}

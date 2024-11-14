d01cf982e33ef3c1778bb68431ba2a417ff24f5b
RehanAnsari765 <ra6369419@gmail.com>
2024-11-14 11:56:21 +0530

    Add files via upload

---
+++ (6) logical operator/logicaloperator.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+void main () {
+
+    int wallet = 500;
+
+    if(wallet >= 500 && wallet <= 1000) {
+        printf("Money is enough");
+    }else {
+        printf("Money is not enough");
+    }
+
+}

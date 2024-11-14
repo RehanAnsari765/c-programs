446d3da9aadd7d5b7af11a11e32f0cef935d7759
RehanAnsari765 <ra6369419@gmail.com>
2024-11-14 11:46:27 +0530

    Add files via upload

---
+++ (1) sums of all signs/first.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main () {
+
+    int a, b, sum;
+
+    printf("Enter a :");
+    scanf("%d", &a);
+
+    printf("Enter b :");
+    scanf("%d", &b);
+
+    sum = a - b;
+
+    printf("sum of a and b is %d", sum);
+
+}
